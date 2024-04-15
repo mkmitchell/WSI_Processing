faa34b6efba7c744e339321640ec9093bf28f100
Michael Mitchell <mmitchell@ducks.org>
2024-04-15 15:00:30 -0400

    Cleanup

--- clipping/combine.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF &SETLOCAL
-SET "Species=nsho"
-set "Myvar=c:\nco\ncrcat.exe "
-for /l %%a in (1979,1,2013) do call set "Myvar=%%Myvar%% WSI_DISPLAY_%%Species%%.%%a.nc"
-call set "Myvar=%%Myvar%% WSI_DISPLAY_%%Species%%.1979_2013.nc"
-ECHO %Myvar%
